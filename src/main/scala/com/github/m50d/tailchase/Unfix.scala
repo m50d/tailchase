d8f10b1357cfcebd0637d2c69eb4ad277b1a74a3
Michael Donaghy <md401@srcf.ucam.org>
2016-02-06 18:29:39 +0000

    Manage case classes
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/tailchase/Unfix.scala
+++ src/main/scala/com/github/m50d/tailchase/Unfix.scala
@@ -5,48 +5,60 @@ import scalaz.Leibniz
 import scalaz.Leibniz.===
 import shapeless._
 
-sealed trait UnfixHelper[F, +Bound, M <: Bound] {
+sealed trait UnfixHelper[F, +Bound, M] {
   type O[A] <: Bound
-  implicit val unfix: Leibniz[Nothing, Bound, M, O[F]]
   implicit val functor: Functor[O]
+  def unfix(m: M): O[F]
+  def fix(of: O[F]): M
 }
 
 trait UnfixHelper2 {
   implicit def deriveA[F, A0] = new UnfixHelper[F, Any, A0] {
     override type O[A] = A0
-    override implicit val unfix = Leibniz.refl[A0]
     override implicit val functor = new Functor[O] {
-      override def map[A, B](fa: O[A])(f: A => B) = fa
+      override def map[A, B](fa: O[A])(f: A ⇒ B) = fa
     }
+    override def unfix(m: A0) = m
+    override def fix(of: A0) = of
   }
 }
 
 trait UnfixHelper1 extends UnfixHelper2 {
   implicit def deriveHNil[F] = new UnfixHelper[F, HNil, HNil] {
     override type O[A] = HNil
-    override implicit val unfix = Leibniz.refl[HNil]
     override implicit val functor = new Functor[O] {
       override def map[A, B](fa: O[A])(f: A ⇒ B) = HNil
     }
+    override def unfix(m: HNil) = HNil
+    override def fix(of: HNil) = HNil
   }
 
   implicit def deriveHCons[F, H, T <: HList](implicit ch: Lazy[UnfixHelper[F, Any, H]], ct: Lazy[UnfixHelper[F, HList, T]]) =
     new UnfixHelper[F, HList, H :: T] {
       override type O[A] = ch.value.O[A] :: ct.value.O[A]
-      override implicit val unfix =
-        Leibniz.lift2[Nothing, Nothing, Nothing, Any, HList, HList, ::, H, ch.value.O[F], T, ct.value.O[F]](ch.value.unfix, ct.value.unfix)
       override implicit val functor: Functor[O] = new Functor[O] {
-        override def map[A, B](fa: O[A])(f: A => B) = ch.value.functor.map(fa.head)(f) :: ct.value.functor.map(fa.tail)(f)
+        override def map[A, B](fa: O[A])(f: A ⇒ B) = ch.value.functor.map(fa.head)(f) :: ct.value.functor.map(fa.tail)(f)
       }
+      override def unfix(m: H :: T) = ch.value.unfix(m.head) :: ct.value.unfix(m.tail)
+      override def fix(of: O[F]) = ch.value.fix(of.head) :: ct.value.fix(of.tail)
+    }
+
+  implicit def deriveInstance[F, G, H <: HList](implicit gen: Generic.Aux[G, H], cg: Lazy[UnfixHelper[F, HList, H]]) =
+    new UnfixHelper[F, HList, G] {
+      override type O[A] = cg.value.O[A]
+      override implicit val functor = cg.value.functor
+      override def unfix(m: G) = cg.value.unfix(gen.to(m))
+      override def fix(of: O[F]) = gen.from(cg.value.fix(of))
     }
 }
 object UnfixHelper extends UnfixHelper1 {
   implicit def deriveF[F] = new UnfixHelper[F, Any, F] {
     override type O[A] = A
-    override implicit val unfix = Leibniz.refl[F]
     override implicit val functor = new Functor[O] {
-      override def map[A, B](fa: O[A])(f: A => B) = f(fa)
+      override def map[A, B](fa: O[A])(f: A ⇒ B) = f(fa)
     }
+    override def fix(f: F) = f
+    override def unfix(f: F) = f
   }
 }
 
--- src/test/scala/com/github/m50d/tailchase/UnfixTest.scala
+++ src/test/scala/com/github/m50d/tailchase/UnfixTest.scala
@@ -4,9 +4,16 @@ import org.junit.Test
 import shapeless._
 import org.fest.assertions.Assertions.assertThat
 
+case class TestCaseClass(f: Float, i: Int)
+
 class UnfixTest {
   @Test def unfixHelperHList(): Unit = {
     val uh = the[UnfixHelper[Int, Any, String :: Int :: HNil]]
     assertThat(uh.functor.map("Hello" :: 4 :: HNil)({i: Int => i / 2.0})).isEqualTo("Hello" :: 2.0 :: HNil)
+  }
+  
+  @Test def unfixHelperCaseClass(): Unit = {
+    val uh = the[UnfixHelper[Float, Any, TestCaseClass]]
+    assertThat(uh.functor.map(2f :: 4 :: HNil)(_.toString)).isEqualTo("2.0" :: 4 :: HNil)
   }
 }
