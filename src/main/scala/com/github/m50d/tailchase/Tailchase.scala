53ab4896cf5eb101fb8770bad2cb85b86d3b4bbd
Michael Donaghy <md401@srcf.ucam.org>
2016-02-06 20:18:55 +0000

    One idea
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/com/github/m50d/tailchase/Tailchase.scala
+++ src/main/scala/com/github/m50d/tailchase/Tailchase.scala
@@ -1,7 +1,11 @@
 package com.github.m50d.tailchase
 
+import shapeless.Poly
+
 object Tailchase {
-  implicit class Tailchaseable[F](val inner: F)(implicit u: UnfixHelper[F, Any, F] with NonTrivial) {
-    def catap[P <: Poly](p: P)
-  }
+//  implicit class Tailchaseable[F](val inner: F)(implicit u: UnfixHelper[F, Any, F] with NonTrivial) {
+//    import u._
+//    def cata[A](f: u.O[A] => A): A =
+//      unfix()
+//  }
 }
